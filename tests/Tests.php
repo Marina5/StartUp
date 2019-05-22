3e396366cb6da6b133c9ab2881a79f5770dbbee6
Yannoël <yannoel.martin@hotmail.fr>
2019-05-22 14:19:37 +0200

    initial

---
+++ tests/Tests.php
@@ -0,0 +1,17 @@
+<?php
+
+require ('Personnages.php');
+require ('Terrain.php');
+require ('Tresor.php');
+
+class Tests extends \PHPUnit\Framework\TestCase {
+    // private $_personnage_lambda;
+
+    public function setUp() {
+    }
+    public function testName() {
+        $this->assertEquals($v1, $v2);
+    }
+    public function tearDown() {
+    }
+}
